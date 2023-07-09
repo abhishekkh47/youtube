0a7f80ef495955ff14a304a5dc2feb01ed727370
Abhishek Khandelwal <abhishekkhandelwal47@gmail.com>
2023-07-09 19:59:01 +0530

    completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,23 +1,35 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import {Sidebar, Videos} from './';
-
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromApi';
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  // we want to fetch data as soon as feed loads
+  // useEffect is a life cycle hook which gets called when the function initially loads
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory])
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className='copyright' variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2022 Abhishek
         </Typography>
       </Box>
       {/* for adding videos */}
-      <Box p={2} sx={{ overflowY:'auto', height:'90vh', flex:2}}>
-        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color:'white'}}>
-          New<span style={{ color: '#F31503' }}>videos</span>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+          {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
 
-        <Videos videos={[]}/>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
